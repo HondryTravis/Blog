5634522de37db31a2fade5fa45d03dda2494c02e
travis <hondry.travis@outlook.com>
2020-05-18 11:50:30 +0800

    update

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -52,7 +52,8 @@ module.exports = {
       title: '趣味算法',
       collapsable: false,
       children:[
-        'other_cards'
+        'other_cards',
+        'range_of_motion'
       ]
     },
   ],
