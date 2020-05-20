fac8297af5ad381c2ad01380f93d5db7cd620313
travis <hondry.travis@outlook.com>
2020-05-20 18:06:48 +0800

    update

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,93 +1,102 @@
+// blog
+const blog = [
+  {
+    title: 'python',
+    collapsable: false,
+    children: [
+      'python/python爬虫'
+    ]
+  },
+  {
+    title: 'nginx',
+    collapsable: false,
+    children: [
+      'nginx/nginx'
+    ]
+  },
+  {
+    title: '数据库',
+    collapsable: false,
+    children: [
+      'mysql/mysql'
+    ]
+  },
+  {
+    title: '常用工具',
+    collapsable: false,
+    children: [
+      'mac/command_line',
+      'mac/homebrew',
+      'mac/nvm',
+      'node/nrm'
+    ]
+  }
+]
+
+// 算法
+const algorithm = [
+  {
+    title: '数组',
+    collapsable: false,
+    children: [
+      'array/count',
+      'array/bisection_method',
+      'array/find_min_number'
+    ]
+  },
+  {
+    title: '字符串',
+    collapsable: false,
+    children: [
+      'string/slide_window'
+    ]
+  },
+  {
+    title: '栈、队列、链表',
+    collapsable: false,
+    children: [
+      'sort/queue',
+      'sort/linked_list'
+    ]
+  },
+  {
+    title: '趣味算法',
+    collapsable: false,
+    children: [
+      'other/cards',
+      'other/range_of_motion'
+    ]
+  },
+]
+
+// 开源相关
+const open_source = [
+  {
+    title: 'TinyDB',
+    collapsable: false,
+    children: [
+      'github/indexeddb',
+      'github/tinydb_docapi'
+    ]
+  },
+  {
+    title: 'Simple-dark',
+    collapsable: false,
+    children: [
+      'vscode/Simple-dark'
+    ]
+  },
+  {
+    title: 'tscli',
+    collapsable: false,
+    children: [
+      'cli/tscli'
+    ]
+  }
+]
+
 module.exports = {
-  '/blog/':[
-    {
-      title: 'python 安装以及使用',
-      collapsable: false,
-      children:[
-        'python爬虫'
-      ]
-    },
-    {
-      title: 'nginx 安装以及使用',
-      collapsable: false,
-      children:[
-        'nginx'
-      ]
-    },
-    {
-      title: '数据库知识',
-      collapsable: false,
-      children:[
-        'mysql'
-      ]
-    },
-    {
-      title: '常用工具，命令行必备',
-      collapsable: false,
-      children:[
-        'nvm',
-        'nrm',
-        'mac',
-        'brew'
-      ]
-    }
-  ],
-  '/algorithm/':[
-    {
-      title: '数组',
-      collapsable: false,
-      children:[
-        'array_count',
-        'array_bisection_method',
-        'array_find_min_number'
-      ]
-    },
-    {
-      title: '字符串',
-      collapsable: false,
-      children:[
-        'string_slide'
-      ]
-    },
-    {
-      title: '栈、队列、链表',
-      collapsable: false,
-      children:[
-        'queue',
-        'linked_list'
-      ]
-    },
-    {
-      title: '趣味算法',
-      collapsable: false,
-      children:[
-        'other_cards',
-        'range_of_motion'
-      ]
-    },
-  ],
-  '/open_source/':[
-    {
-      title: 'TinyDB',
-      collapsable: false,
-      children:[
-        'indexeddb',
-        'tinydb_docapi'
-      ]
-    },
-    {
-      title: 'Simple-dark',
-      collapsable: false,
-      children:[
-        'Simple-dark'
-      ]
-    },
-    {
-      title: 'tscli',
-      collapsable: false,
-      children:[
-        'tscli'
-      ]
-    }
-  ]
+  '/blog/': blog,
+  '/algorithm/': algorithm,
+  '/open_source/': open_source
 }
